0c9f77ea1d56bf5b298bf3ca01fdacfdc46a2783
MichaelRoss <msross@dundee.ac.uk>
2013-03-18 22:45:50 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HonsProject::Application.config.secret_token = 'a739c1826cb7ffe2de5c69cf348be3e2c5b04bade7897ee6bb5f80116b8b4dac438ea8d6e3bffb96e95b045930237965e9b91563e4fa0d3385c3142d4eeca628'
